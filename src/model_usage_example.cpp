d4b083b367bb02f17acedbd62509ee94208a07a1
gp-1108 <pietrogirotto00@gmail.com>
2023-07-17 14:05:28 +0200

    polished

--- src/model_usage_example.cpp
+++ src/model_usage_example.cpp
@@ -3,17 +3,24 @@
 #include <opencv2/highgui.hpp>
 
 /*
-A simple main for displaying the model usage
+  A simple main for displaying the model usage
 */
 int main(int argc, char **argv)
 {
-  if (argc < 2)
+  if (argc < 3)
   {
-    std::cout << "Usage: " << argv[0] << " <path_to_model>" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <path_to_model> <path_to_image>" << std::endl;
     return 1;
   }
+
   ImagePredictor predictor(argv[1]);
+
   cv::Mat image = cv::imread(argv[2], cv::IMREAD_COLOR);
+  if (image.empty())
+  {
+    std::cout << "Error loading the image" << std::endl;
+    return 1;
+  }
 
   std::vector<double> predictions = predictor.predict(image);
 
@@ -22,6 +29,5 @@ int main(int argc, char **argv)
     std::cout << "Prediction for class " << predictor.get_label(i) << ": " << predictions[i] << std::endl;
   }
 
-  // Reading all the images 
   return 0;
 }

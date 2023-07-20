a3ca3de884c25074176993921b6dafc7d2e00a34
nicktex <130552806+nicktex@users.noreply.github.com>
2023-07-20 20:08:44 +0200

    Update dish_detector.h

--- src/dish_detector/dish_detector.h
+++ src/dish_detector/dish_detector.h
@@ -6,5 +6,6 @@
 
 bool dishDetector(cv::Mat& tray, cv::Mat& cmp_tray_mask, std::vector<std::vector<int>>& bb, ImagePredictor& predictor, std::vector<double>& confidenceVector);
 void segment_dishes(const cv::Mat& plate, cv::Point top_left, cv::Mat& cmp_tray_mask, std::vector<std::vector<int>>& bb, ImagePredictor& predictor, std::vector<double>& confidenceVector);
+void search_template(const Mat& dish_template, Mat& search_image, Mat& output_mask);
 
 #endif  // DISH_DETECTOR_H
